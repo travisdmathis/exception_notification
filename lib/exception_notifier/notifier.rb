b8a42483118c6fb4d85383df9b858307317b749e
Travis Mathis <travis@imagerelay.com>
2016-01-29 09:48:36 -0500

    Update notifier.rb

--- lib/exception_notifier/notifier.rb
+++ lib/exception_notifier/notifier.rb
@@ -2,6 +2,24 @@ require 'active_support/deprecation'
 
 module ExceptionNotifier
   class Notifier
+    
+    ExceptionNotifier::Notifier.class_eval do
+      #https://github.com/smartinez87/exception_notification/blob/master/lib/exception_notifier/notifier.rb
+      def self.exception_notification(*args)
+        message = super
+        
+        _limit = 5.minutes.ago
+        @@last_notification||=_limit 
+        if @@last_notification > _limit
+          Rails.logger.info "ExceptionNotifier rate limit triggered, #{ExceptionNotifier::Notifier.deliveries.size} notifications limited."
+          message.delivery_method :test
+        else
+          @@last_notification = Time.now
+        end
+        
+        message
+      end
+    end
 
     def self.exception_notification(env, exception, options={})
       ActiveSupport::Deprecation.warn "Please use ExceptionNotifier.notify_exception(exception, options.merge(:env => env))."
